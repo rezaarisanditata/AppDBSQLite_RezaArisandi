564d4c626e367d30f9a22ae768804bd384671bea
rezaarisanditata <arisandizn@gmail.com>
2022-06-14 00:22:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appdbsqlite_rezaarisandi/MataKuliah.kt
@@ -0,0 +1,8 @@
+package com.example.appdbsqlite_rezaarisandi
+
+class MataKuliah (
+    var kdMatkul : String,
+    var NmMatkul : String,
+    var sks : Int,
+    var sifat: String,
+        )
---
+++ app/src/main/java/com/example/appdbsqlite_rezaarisandi/layout_item.kt
@@ -0,0 +1,11 @@
+package com.example.appdbsqlite_rezaarisandi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_item)
+    }
+}
